0933c56c07bbc64882627cd5664505e156ff4c6b
XIVN1987 <XIVN1987@163.com>
2025-08-22 16:17:48 +0800

    add SWM341 test

---
+++ SWM341/APP/main.c
@@ -0,0 +1,46 @@
+#include "SWM341.h"
+
+
+void SerialInit(void);
+
+
+void hw_init(void)
+{
+ 	SystemInit();
+	
+	SerialInit();
+	
+	TIMR_Init(BTIMR0, TIMR_MODE_TIMER, CyclesPerUs, 0xFFFFFF, 0);
+	TIMR_Start(BTIMR0);
+}
+
+
+void SerialInit(void)
+{
+	UART_InitStructure UART_initStruct;
+	
+	PORT_Init(PORTM, PIN0, PORTM_PIN0_UART0_RX, 1);
+	PORT_Init(PORTM, PIN1, PORTM_PIN1_UART0_TX, 0);
+ 	
+ 	UART_initStruct.Baudrate = 57600;
+	UART_initStruct.DataBits = UART_DATA_8BIT;
+	UART_initStruct.Parity = UART_PARITY_NONE;
+	UART_initStruct.StopBits = UART_STOP_1BIT;
+	UART_initStruct.RXThreshold = 3;
+	UART_initStruct.RXThresholdIEn = 0;
+	UART_initStruct.TXThreshold = 3;
+	UART_initStruct.TXThresholdIEn = 0;
+	UART_initStruct.TimeoutTime = 10;
+	UART_initStruct.TimeoutIEn = 0;
+ 	UART_Init(UART0, &UART_initStruct);
+	UART_Open(UART0);
+}
+
+int fputc(int ch, FILE *f)
+{
+	UART_WriteByte(UART0, ch);
+	
+	while(UART_IsTXBusy(UART0));
+ 	
+	return ch;
+}
